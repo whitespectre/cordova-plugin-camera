308c24817c9c9c567c60fed208ce4738d788c872
Jose Granja Martinez <dioxmio@gmail.com>
2017-05-16 14:31:34 +0200

    CGA-3837
    requesting permissions on recording video

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -144,7 +144,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
         this.applicationId = preferences.getString("applicationId", this.applicationId);
 
         if (action.equals("permissions")) {
-          PermissionHelper.requestPermissions(this, REQUEST_PERMISSIONS, new String[] {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO});
+          PermissionHelper.requestPermissions(this, REQUEST_PERMISSIONS, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE});
           return true;
         }
 
